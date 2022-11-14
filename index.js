82747d16459f52a535b1098c4d7d7981bf19fbdf
brandonTsai <brandonya125@gmail.com>
2022-11-14 00:39:42 -0800

    Get the featured products in home page

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const init = async () => {
   if (products) {
     //add products to the store
     setupStore(products)
+    const featured = store.filter((product) => product.featured)
+    console.log(featured)
   }
 }
 
