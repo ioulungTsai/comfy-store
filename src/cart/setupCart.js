0e1cdd1f17b83ad71dd7b3d82d9832582939bc9e
brandonTsai <brandonya125@gmail.com>
2022-11-17 00:19:45 -0800

    Add remove functionality on each item in the cart

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -63,6 +63,10 @@ function displayCartItemsDOM() {
   })
 }
 
+function removeItem(id) {
+  cart = cart.filter((cartItem) => cartItem.id !== id)
+}
+
 function increaseAmount(id) {
   let newAmount
   cart = cart.map((cartItem) => {
@@ -75,7 +79,25 @@ function increaseAmount(id) {
   return newAmount
 }
 
-function setupCartFunctionality() {}
+function setupCartFunctionality() {
+  cartItemsDOM.addEventListener("click", function (e) {
+    const element = e.target
+    const parent = e.target.parentElement
+    const id = e.target.dataset.id
+    const parentID = e.target.parentElement.dataset.id
+    // remove
+    if (element.classList.contains("cart-item-remove-btn")) {
+      removeItem(id)
+      parent.parentElement.remove()
+    }
+    // increase
+    // decrease
+
+    displayCartItemCount()
+    displayCartTotal()
+    setStorageItem("cart", cart)
+  })
+}
 
 const init = () => {
   // display amount of cart items
