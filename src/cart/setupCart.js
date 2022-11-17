054fa3568a9ab5922db130e8c5273a2087c5888e
brandonTsai <brandonya125@gmail.com>
2022-11-16 23:49:44 -0800

    Update item amount in cart when it exist already

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -28,6 +28,10 @@ export const addToCart = (id) => {
     console.log(cart)
   } else {
     // update values
+    const amount = increaseAmount(id)
+    const items = [...cartItemsDOM.querySelectorAll(".cart-item-amount")]
+    const newAmount = items.find((value) => value.dataset.id === id)
+    newAmount.textContent = amount
   }
   // add one to the item count
   displayCartItemCount()
@@ -58,6 +62,19 @@ function displayCartItemsDOM() {
     addToCartDOM(cartItem)
   })
 }
+
+function increaseAmount(id) {
+  let newAmount
+  cart = cart.map((cartItem) => {
+    if (cartItem.id === id) {
+      newAmount = cartItem.amount + 1
+      cartItem = { ...cartItem, amount: newAmount }
+    }
+    return cartItem
+  })
+  return newAmount
+}
+
 function setupCartFunctionality() {}
 
 const init = () => {
